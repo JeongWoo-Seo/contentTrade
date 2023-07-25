dac26cf9c349586d6f9732b371b4c48f70f8d5ea
JeongWoo-Seo <sjwoo6500@naver.com>
2023-07-25 21:13:47 +0900

    init commit

---
+++ server/src/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const server = async () =>{
+    const app = express();
+    app.set('port',8000);
+    app.get('/',(req,res) => {
+        res.send("hello");
+    });
+
+    app.listen(app.get('port'));
+}
+
+export default server;
